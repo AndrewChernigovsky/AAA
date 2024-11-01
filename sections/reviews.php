2ff8ba023d8ee343f2ed826864d0779b43b5b24a
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-11-01 16:38:11 +0300

    reviews

---
+++ sections/reviews.php
@@ -0,0 +1,22 @@
+<section class="reviews">
+  <div class="container">
+    <ul>
+      <li><img src="" alt="аватар">
+        <h4>Имя Фамилия</h4>
+        <p>Описание</p>
+      </li>
+      <li><img src="" alt="аватар">
+        <h4>Имя Фамилия</h4>
+        <p>Описание</p>
+      </li>
+      <li><img src="" alt="аватар">
+        <h4>Имя Фамилия</h4>
+        <p>Описание</p>
+      </li>
+      <li><img src="" alt="аватар">
+        <h4>Имя Фамилия</h4>
+        <p>Описание</p>
+      </li>
+    </ul>
+  </div>
+</section>
