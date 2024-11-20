cca57ac38bccf43bf22150b49d7d39912daf2528
Andrew Izosimov <chernigovsky108@gmail.com>
2024-11-21 02:04:12 +0300

    content

--- src/js/modules/simple-form.js
+++ src/js/modules/simple-form.js
@@ -33,8 +33,14 @@ window.onload = function () {
 
 function sendForm(formId, recaptchaResponse) {
   const form = document.getElementById(formId);
-  const phone = formSimple.querySelector("input[name='user-tel']");
-  const messengers = formSimple.querySelectorAll("input[name='social']");
+
+
+  const phone = () => {
+    if (form) return form.querySelector("input[name='user-tel']");
+  }
+  const messengers = () => {
+    if (form) return form.querySelectorAll("input[name='social']");
+  }
 
   function validation() {
     let isChecked = false;
@@ -48,54 +54,55 @@ function sendForm(formId, recaptchaResponse) {
       messengerValid: isChecked,
     };
   }
+  if (form) {
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+      const validationResult = validation();
 
-  formSimple.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const validationResult = validation();
-
-    if (hiddenInput.value.length === 0 && validationResult.phoneValid && validationResult.messengerValid) {
-      if (!recaptchaResponse) {
-        popup.classList.add('active');
-        popup.querySelector('h3').textContent = "Нужно пройти капчу";
-        setTimeout(() => popup.classList.remove('active'), 3000);
-        return;
-      } else {
-        const formData = new FormData(form);
-        formData.append('g-recaptcha-response', recaptchaResponse);
-        const action = form === 'form-simple' ? './php/functions/mail/mail.php' : './php/functions/mail/mail-simple.php';
-        console.log(action, 'ACTION');
-        grecaptcha.reset(widgetId1);
-        grecaptcha.reset(widgetId2);
-        recaptchaResponse = undefined;
-        fetch(action, {
-          method: 'POST',
-          body: formData
-        })
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
-            console.log(123123123);
-            popup.classList.add('active');
-            popup.querySelector('h3').textContent = 'Данные успешно отправлены!';
-            popup.querySelector('p').textContent = 'Сообщение закроется через 3 секунды';
-            setTimeout(() => popup.classList.remove('active'), 3000);
+      if (hiddenInput.value.length === 0 && validationResult.phoneValid && validationResult.messengerValid) {
+        if (!recaptchaResponse) {
+          console.log(11111);
+          popup.classList.add('active');
+          popup.querySelector('h3').textContent = "Нужно пройти капчу";
+          setTimeout(() => popup.classList.remove('active'), 3000);
+          return;
+        } else {
+          const formData = new FormData(form);
+          formData.append('g-recaptcha-response', recaptchaResponse);
+          const action = form === 'form-simple' ? './php/functions/mail/mail.php' : './php/functions/mail/mail-simple.php';
+          grecaptcha.reset(widgetId1);
+          grecaptcha.reset(widgetId2);
+          recaptchaResponse = undefined;
+          fetch(action, {
+            method: 'POST',
+            body: formData
           })
-          .catch(error => console.error('Ошибка:', error));
+            .then(response => {
+              if (!response.ok) {
+                throw new Error('Network response was not ok');
+              }
+              popup.classList.add('active');
+              popup.querySelector('h3').textContent = 'Данные успешно отправлены!';
+              popup.querySelector('p').textContent = 'Сообщение закроется через 3 секунды';
+              setTimeout(() => popup.classList.remove('active'), 3000);
+            })
+            .catch(error => console.error('Ошибка:', error));
+        }
+      } else {
+        if (!validationResult.phoneValid) {
+          popup.classList.add('active');
+          popup.querySelector('h3').textContent = "Введите номер телефона";
+          setTimeout(() => popup.classList.remove('active'), 3000);
+        }
+        if (!validationResult.messengerValid) {
+          popup.classList.add('active');
+          popup.querySelector('h3').textContent = "Выберите мессенджер";
+          setTimeout(() => popup.classList.remove('active'), 3000);
+        }
       }
-    } else {
-      if (!validationResult.phoneValid) {
-        popup.classList.add('active');
-        popup.querySelector('h3').textContent = "Введите номер телефона";
-        setTimeout(() => popup.classList.remove('active'), 3000);
-      }
-      if (!validationResult.messengerValid) {
-        popup.classList.add('active');
-        popup.querySelector('h3').textContent = "Выберите мессенджер";
-        setTimeout(() => popup.classList.remove('active'), 3000);
-      }
-    }
-  });
+    });
+  }
+
 }
 
 export function initFormSimple() {
--- src/pages/services/landing.php
+++ src/pages/services/landing.php
@@ -8,10 +8,10 @@ include_once './../../helpers/includeSections.php';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ААА Академия Андрея Андреевича Изосимова, создание и продвижение сайтов | Лендинг</title>
-  <link rel="stylesheet" href="/dist/assets/libs/libs.css">
-  <link rel="stylesheet" href="/dist/css/style.css">
-  <script src="/dist/assets/libs/libs.js" type="module" defer></script>
-  <script src="/dist/js/main.js" type="module" defer></script>
+  <link rel="stylesheet" href="/assets/libs/libs.css">
+  <link rel="stylesheet" href="/css/style.css">
+  <script src="/assets/libs/libs.js" type="module" defer></script>
+  <script src="/js/main.js" type="module" defer></script>
 </head>
 
 <body>
